424ae2e609e4aebf233f46bff605ab965a69b1e0
khromov <khromov.d@oprosso.net>
2024-09-03 22:09:06 +0300

    execute lesson 1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,146 @@
+import java.util.Arrays;
+
 public class Application {
 
     public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+        System.out.println(isSum(10, 2));
+        checkSignArgs(6);
+        System.out.println(getSignArgs(2));
+        printWords("Hello world", 2);
+        System.out.println(isLeapYear(400));
+        System.out.println(Arrays.toString(swapNumbers(new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0})));
+        System.out.println(Arrays.toString(fill()));
+        System.out.println(Arrays.toString(multiply()));
+        matrix();
+        System.out.println(Arrays.toString(createArray(5, 1)));
+    }
 
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 2;
+        int b = 3;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 4;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 2;
+        int b = 3;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    public static boolean isSum(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
+
+    public static void checkSignArgs(int a) {
+        if (a >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    public static boolean getSignArgs(int a) {
+          return a < 0;
+    }
+
+    public static void printWords(String word, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(word);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else return year % 4 == 0;
+    }
+
+    public static int[] swapNumbers(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                array[i] = 1;
+            } else {
+                array[i] = 0;
+            }
+        }
+        return array;
+    }
+
+    public static int[] fill() {
+        int[] array = new int[100];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        return array;
+    }
+
+    public static int[] multiply() {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
+            }
+        }
+        return array;
+    }
+
+    public static void matrix() {
+        int size = 5;
+        int[][] matrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            matrix[i][i] = 1;
+            matrix[i][size - 1 - i] = 1;
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[] createArray(int len, int initialValue) {
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++) {
+            array[i] = initialValue;
+        }
+        return array;
     }
 }
